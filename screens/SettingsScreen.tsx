4953b74f108e14d79a76f879998e1a76c9d0f07d
mjamry <michal.jamry@gmail.com>
2024-01-16 19:23:32 +0100

    Added slider along with usage on settings page

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -6,6 +6,7 @@ import { Canvas, Circle, Fill, Path, ImageSVG, Skia, Group, fitbox, rect, FitBox
 import Constants from 'expo-constants';
 import { useNavigation } from '@react-navigation/native';
 import IconButton from '../components/IconButton';
+import Slider from '../components/Slider';
 
 const svg = Skia.SVG.MakeFromString(
   `<svg viewBox='0 0 20 20' width="20" height="20" xmlns='http://www.w3.org/2000/svg'>
@@ -21,16 +22,43 @@ const SettingsScreen = () => {
   const work = useTimersStore(s => s.work);
   const shortBreak = useTimersStore(s => s.shortBreak);
   const longBreak = useTimersStore(s => s.longBreak);
+  const setWork = useTimersStore(s => s.setWork);
+  const setShortBreak = useTimersStore(s => s.setBreak);
+  const setLongBreak = useTimersStore(s => s.setLongBreak);
   const nav = useNavigation();
 
   return (
     <>
-      <View style={{ flex: 1, paddingTop: Constants.statusBarHeight, backgroundColor: "green"}}>
-        <Text>Work {work}</Text>
-        <Text>short break {shortBreak}</Text>
-        <Text>long break {longBreak}</Text>
+      <View style={{ flex: 1, paddingTop: Constants.statusBarHeight * 2, backgroundColor: "green"}}>
         <View style={{margin: 20, position: 'absolute', top: Constants.statusBarHeight, right: 0}}>
           <IconButton size="medium" type="clock" onPress={() => nav.navigate('Home')} />
+        </View>
+        <View style={{marginTop: Constants.statusBarHeight + 40, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <Slider 
+            width={200}
+            step={5}
+            range={{min: 10, max: 60}}
+            initialValue={25}
+            onChange={(value) => setWork(value)}
+            title="work time"/>
+        </View>
+        <View style={{marginTop: 40, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <Slider 
+            width={200}
+            step={1}
+            range={{min: 1, max: 10}}
+            initialValue={5}
+            onChange={(value) => setShortBreak(value)}
+            title="short break"/>
+        </View>
+        <View style={{marginTop: 40, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <Slider 
+            width={200}
+            step={5}
+            range={{min: 5, max: 25}}
+            initialValue={5}
+            onChange={(value) => setLongBreak(value)}
+            title="long break"/>
         </View>
       </View>
     </>
