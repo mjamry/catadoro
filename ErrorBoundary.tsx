357f167974502aab6e1b13e434d117a546ea5382
mjamry <michal.jamry@gmail.com>
2024-03-22 13:43:45 +0100

    #4 Wrap error boundary with function component to use hooks and store error in logs

--- ErrorBoundary.tsx
+++ ErrorBoundary.tsx
@@ -1,31 +1,35 @@
 import React, { Component, ErrorInfo, ReactNode } from "react";
 import ErrorScreen from './screens/ErrorScreen';
+import useLoggerService from "./services/logger/LoggerService";
 
-interface Props {
+interface DefaultProps {
   children?: ReactNode;
+  onError: (error: Error, errorInfo: ErrorInfo) => void;
 }
 
 interface State {
   hasError: boolean;
+  error: string;
 }
 
-class ErrorBoundary extends Component<Props, State> {
+class DefaultErrorBoundary extends Component<DefaultProps, State> {
   public state: State = {
-    hasError: false
+    hasError: false,
+    error: ''
   };
 
-  public static getDerivedStateFromError(_: Error): State {
+  public static getDerivedStateFromError(e: Error): State {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, error: e.message};
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error("Uncaught error:", error, errorInfo);
+    this.props.onError(error, errorInfo);
   }
 
   public render() {
     if (this.state.hasError) {
-      return <ErrorScreen />
+      return <ErrorScreen error={this.state.error} />
     }
 
     return (
@@ -33,5 +37,25 @@ class ErrorBoundary extends Component<Props, State> {
     )
   }
 }
+
+type ErrorBoundaryProps = {
+  children?: ReactNode;
+}
+
+const ErrorBoundary = (props: ErrorBoundaryProps) => {
+  const { children } = props;
+  const logger = useLoggerService('EB');
+
+  const handleError = (error: Error, errorInfo: ErrorInfo) => {
+    logger.error(error.message, error, errorInfo);
+  }
+
+  return (
+    <DefaultErrorBoundary onError={handleError}>
+      {children}
+    </DefaultErrorBoundary>
+  );
+
+};
 
 export default ErrorBoundary;
